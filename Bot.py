98b5f6a32f2fd7b1c442753420e1cd65f7b32fc2
NazarKostetskiy <31857245+NazarKostetskiy@users.noreply.github.com>
2019-03-25 20:06:50 +0200

    Bot
    object bot with minimal functions created

---
+++ Bot.py
@@ -0,0 +1,22 @@
+import telebot
+
+
+
+class bot:
+    bot = telebot.TeleBot('709037271:AAFPMYU-Jxbc4DOMDBkdl6PR5V_G8GbNFcQ')
+    updates = bot.get_updates()
+    def __call__(self, bot=bot):
+        @bot.message_handler(commands=['start', 'help'])
+        def send_welcome(message):
+            bot.reply_to(message, '''Виртуальная служба скорой помощи города Зеон. Обращайтесь только в случае крайней необходимоси.
+              \nНаш бот запишет Ваше обращение и пошлет к Вам скорую помощь.
+              \n       Для продолжения нажмите /continue''')
+
+        @bot.message_handler(commands=['continue'])
+        def sypthoms(message):
+            bot.send_message(message.chat.id, 'Опишите симпотмы:')
+        bot.polling()
+
+
+Bot = bot().__call__()
+
